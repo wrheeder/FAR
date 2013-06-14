40849afe72ad243dcc58509f0c77fb090e0ee7c1
Willem Rheeder <wrheeder@cellc.co.za>
2013-06-14 13:43:32 +0200

    Testing Commit and push to production

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
 // agile_project - this is realm. It should be unique per-project
 // jui - this is theme. Keep it jui unless you want to make your own theme
 //set_include_path(get_include_path(). PATH_SEPARATOR .'/home/nsnexter/php/');
-require_once 'Spreadsheet/Excel/Writer.php';
+//require_once 'Spreadsheet/Excel/Writer.php';
 include 'atk4/loader.php';
 
 date_default_timezone_set('Africa/Johannesburg');
-$api=new Frontend('eclipse','cellc');
+$api=new Frontend('FAR','CellC');
 $api->main();
 ?>
